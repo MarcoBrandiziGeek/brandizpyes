a6554962ba0ceb072fecb69b4c3ae46f662b49e1
Marco Brandizi <marco.brandizi@gmail.com>
2025-10-02 16:26:36 +0100

    Adding a few comments.

--- src/brandizpyes/logging.py
+++ src/brandizpyes/logging.py
@@ -3,10 +3,15 @@ import os
 from sys import stderr
 import yaml
 
+# TODO: support the rich library components, to get coloured output
+# see https://chatgpt.com/share/68de70b6-2904-800d-a57a-54ede673f531
+# 
+
 def logger_config ( 
-	logger_name = None, cfg_path = None, 
-	disable_existing_loggers = False,
-	use_unsafe_loader = False
+	logger_name: str = None,
+	cfg_path: str = None, 
+	disable_existing_loggers: bool = False,
+	use_unsafe_loader: bool = False
 ) -> logging.Logger | None:
 	"""
 	Configures the Python logging module with a YAML configuration file.
@@ -14,7 +19,7 @@ def logger_config (
 	The file name is picked, in order: from cfg_path if provided, from the environment variable 
 	PYES_LOG_CONF, from <current directory>/logging-test.yml or <current directory>/logging.yml.
 
-	This should be called at the begin of a main program and BEFORE any use of the logging module.
+	This should be called at the begin of an application and BEFORE any use of the logging module.
 	Multiple calls of this method are idempotent, ie, the Python logging module configures itself
 	once only (and only before sending in logging messages).
 	
@@ -24,12 +29,12 @@ def logger_config (
 	to avoid the need to import logging too, when you already import this. Beware that you load a configuration
 	one only in your application (so, don't use this method in modules just to get a logger). 
 	
-	param disable_existing_loggers is false by default, this is the best way to not interfere with modules instantiating
+	:param disable_existing_loggers: is false by default, this is the best way to not interfere with modules instantiating
 	their own module logger, usually before you call this function on top of your application (but usually after 
 	all the imports). By default, the Python logging library has this option set to true and that typically causes
 	all the module loggers to be disabled after the configuration loading. See https://docs.python.org/3/library/logging.config.html
 
-	param use_unsafe_loader: if true, uses yaml.UnsafeLoader to load the configuration file. 
+	:param use_unsafe_loader: if true, uses yaml.UnsafeLoader to load the configuration file. 
 	This is useful when you want to do things like calling functions in the configuration file 
 	(see logging-explicitly-loaded.yml in the tests). However, it's False by default, since this behaviour is
 	unsafe (see Python documentation).
