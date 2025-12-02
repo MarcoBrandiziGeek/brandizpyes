25c59b98f169413400af8d5ac5d185a83404b663
Marco Brandizi <marco.brandizi@gmail.com>
2025-12-02 23:56:43 +0000

    config: add logging to the project tests

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+from brandizpyes.logging import logger_config
+
+"""
+Pytest configuration file, which the framework picks up at startup.
+
+[Details here](https://docs.pytest.org/en/stable/reference/fixtures.html)
+
+"""
+def pytest_configure ( config ):
+	"""
+	As per their docs, this is picked up by pytest at startup.
+	"""
+
+	# We can't use the usual logging-test.yml inside our own tests, since this is already used to
+	# test the brandizpyes.logging module.
+	#
+	cfg_path = os.path.dirname ( __file__ ) + "/logging-brandizpyes-test.yml"
+	logger_config ( __name__, cfg_path )
+
