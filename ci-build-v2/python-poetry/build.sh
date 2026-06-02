dc69271822560ac6f02ce975b9fa82ccfc747409
Marco Brandizi <marco.brandizi@gmail.com>
2026-06-02 12:14:04 +0100

    ci: point CI URL to the new CI Build repo

--- ci-build-v2/python-poetry/build.sh
+++ ci-build-v2/python-poetry/build.sh
@@ -3,7 +3,8 @@ set -eE -o pipefail
 
 
 printf "== Installing ci-build scripts and then running the build\n"
-ci_build_url_base="https://raw.githubusercontent.com/Rothamsted/knetminer-common/refs/heads/ci-build-v2"
+ci_build_version='1.1'
+ci_build_url_base="https://github.com/KnetMiner/knetminer-ci/blob/$ci_build_version"
 script_url="$ci_build_url_base/ci-build-v2/install.sh"
 . <(curl --fail-with-body -o - "$script_url") "$ci_build_url_base" python-poetry
 
