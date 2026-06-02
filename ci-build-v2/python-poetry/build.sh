c34ca03858a35efd0d4f8414a076238dc33d1758
Marco Brandizi <marco.brandizi@gmail.com>
2026-06-02 12:18:03 +0100

    fix (build): fix bad URL in CI/build script

--- ci-build-v2/python-poetry/build.sh
+++ ci-build-v2/python-poetry/build.sh
@@ -4,7 +4,7 @@ set -eE -o pipefail
 
 printf "== Installing ci-build scripts and then running the build\n"
 ci_build_version='1.1'
-ci_build_url_base="https://github.com/KnetMiner/knetminer-ci/blob/$ci_build_version"
+ci_build_url_base="https://raw.githubusercontent.com/KnetMiner/knetminer-ci/refs/tags/$ci_build_version"
 script_url="$ci_build_url_base/ci-build-v2/install.sh"
 . <(curl --fail-with-body -o - "$script_url") "$ci_build_url_base" python-poetry
 
